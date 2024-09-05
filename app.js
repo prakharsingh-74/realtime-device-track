84d75a9844949a85dd06ea23e26364b8322c5483
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-09-05 17:22:35 +0530

    chore: Integrate socket.io for real-time location updates
    Update app.js and script.js to integrate socket.io for real-time location updates. Add event listeners for sending and receiving location data, and emit corresponding events to update the map with markers for each user's location. Also handle disconnection events to remove markers when a user disconnects.

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "/public")));
 
 io.on("connection", function(socket){
-    console.log("connected");
+    socket.on("send-location", function(data){
+        io.emit("recieve-location", {id:socket.id, ...data});
+    })
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id);
+    })
 })
 
 app.get("/", function(req, res) {
--- public/js/script.js
+++ public/js/script.js
@@ -19,5 +19,24 @@ if (navigator.geolocation) {
 const map = L.map("map").setView([0, 0], 10);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "OpenStreetMap"
+    attribution: "Made with ❤️ by Prakhar Singh"
 }).addTo(map);
+
+const markers = {};
+
+socket.on("revieve-location", (data)=>{
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude], 10);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map)
+    }
+})
+
+socket.on("user-disconnected", (id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
